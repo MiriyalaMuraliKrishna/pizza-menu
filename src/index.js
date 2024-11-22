edd5b72c7880f1905529bb4c6d0fd28c6fa5be3c
MuraliJbk <murali@jbkinfotech.com>
2024-11-22 20:59:07 +0530

    teststs

--- src/index.js
+++ src/index.js
@@ -4,16 +4,12 @@ import "./index.css";
 
 import "bootstrap/dist/css/bootstrap.min.css";
 import App from "./App";
-import Header from "./components/Header";
-import Footer from "./components/Footer";
 import reportWebVitals from "./reportWebVitals";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <Header />
     <App />
-    <Footer />
   </React.StrictMode>
 );
 
