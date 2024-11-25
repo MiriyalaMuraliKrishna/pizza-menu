5ecf45a5e8feb729ca9663071171fb6f7f53e1f2
MIRIYALA MURALI KRISHNA <miriyalamuralikrishna@MIRIYALAs-MacBook-Air.local>
2024-11-25 17:53:19 +0530

    rendarlist using map method

--- src/components/Test.jsx
+++ src/components/Test.jsx
@@ -1,4 +1,36 @@
 import React from "react";
+let CourseData = [
+    {
+        courseName: "JavaScript Fundamentals",
+        instructor: "John Doe",
+        duration: "30 hours",
+        fee: 299
+    },
+    {
+        courseName: "React JS",
+        instructor: "Jane Smith",
+        duration: "45 hours",
+        fee: 499
+    },
+    {
+        courseName: "HTML Basics",
+        instructor: "Alice Johnson",
+        duration: "20 hours",
+        fee: 199
+    },
+    {
+        courseName: "CSS Essentials",
+        instructor: "Robert Brown",
+        duration: "25 hours",
+        fee: 249
+    },
+    {
+        courseName: "jQuery for Beginners",
+        instructor: "Emily Davis",
+        duration: "15 hours",
+        fee: 149
+    }
+];
 
 // function Test(){
 //     return <div>This is tesst component</div>
@@ -23,16 +55,25 @@ function Intro(){
     </div>
 }
 function SkillList(){
+  
     return <div className="flex skill-list">
-        <Skill name="React" emoji="💪" color="yellow"/>
+        {/* <Skill name="React" emoji="💪" color="yellow"/>
         <Skill name="javascript" emoji="💪"/>
         <Skill name="html/css" emoji="💪" color="yellow" />
-        <Skill name="jQuery" emoji="💪"/>
+        <Skill name="jQuery" emoji="💪"/> */}
+
+        {
+            CourseData.map((ele)=>
+                <Skill courseObj={ele} key={ele.courseName}  />
+            
+           ) 
+        }
     </div>
 }
 function Skill(props){
-    return <span style={{backgroundColor: props.color, padding:"0.5rem", borderRadius: "6px"}}>
-        <strong>{props.name}</strong>
-        <strong>{props.emoji}</strong>
+    return <span style={{backgroundColor: "yellow", padding:"0.5rem", borderRadius: "6px"}}>
+        <strong>{props.courseObj.courseName}</strong><br />
+        <strong>{props.courseObj.instructor}</strong><br/>
+        <strong>{props.courseObj.fee} '/- rs'</strong>
     </span>
 }
